6ae5baad8b04ac02aa694f4ecce69e5ed6b2bfb0
dzoliana <dzolicana@gmail.com>
2024-01-17 14:09:41 +0100

    getServerSideProps, using context

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,24 @@ function MeetupDetails() {
     />
   );
 }
+
+export async function getStaticProps(context) {
+  //fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image: "https://croti.blog/wp-content/uploads/2023/06/DSC02323.webp",
+        id: meetupId,
+        title: "A First Meetup",
+        address: "Some Street 10, Some City",
+        description: "This is the first meetup",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
